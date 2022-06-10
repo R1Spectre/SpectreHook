b529492ab97ac64bf41e9c5350ed9a42e4153254
Barnaby <22575741+barnabwhy@users.noreply.github.com>
2022-06-11 00:14:37 +0100

    Enable version func

--- hook/version.cpp
+++ hook/version.cpp
@@ -7,13 +7,11 @@ const char* SPECTRE_VERSION = "0.0.1-beta";
 // string function R1SGetVersion()
 SQRESULT SQ_GetVersion(HSQUIRRELVM sqvm)
 {
-	Log::Info("Here?");
 	sq_pushstring(sqvm, SPECTRE_VERSION, -1);
-	Log::Info("Here?");
 	return 1;
 }
 
 void InitialiseVersion(HMODULE baseAddress)
 {
-	//g_SquirrelManager->AddFuncRegistration(SCRIPT_CONTEXT_UI, "R1SGetVersion", &SQ_GetVersion, ".", 0, "string", "", "Gets the Spectre version string");
+	g_SquirrelManager->AddFuncRegistration(SCRIPT_CONTEXT_UI, "R1SGetVersion", &SQ_GetVersion, ".", 0, "string", "", "Gets the Spectre version string");
 }
